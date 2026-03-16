82d7bdfe00270df65dd101662d6bdcf344a8790d
Yoch1x <wellingtonfelmacbat@gmail.com>
2026-03-16 20:38:42 -0300

    ...

---
+++ .next/static/chunks/app/layout.js
@@ -0,0 +1,39 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["app/layout"],{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5Cwelli%5CDocuments%5CPrograma%C3%A7%C3%A3o%5CVS%20Code%5CRepositorios%20Git%20Hub%5CLinkey%5Cnode_modules%5Cremixicon%5Cfonts%5Cremixicon.css&server=false!":
+/*!***************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5Cwelli%5CDocuments%5CPrograma%C3%A7%C3%A3o%5CVS%20Code%5CRepositorios%20Git%20Hub%5CLinkey%5Cnode_modules%5Cremixicon%5Cfonts%5Cremixicon.css&server=false! ***!
+  \***************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./node_modules/remixicon/fonts/remixicon.css */ \"(app-pages-browser)/./node_modules/remixicon/fonts/remixicon.css\"))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz1DJTNBJTVDVXNlcnMlNUN3ZWxsaSU1Q0RvY3VtZW50cyU1Q1Byb2dyYW1hJUMzJUE3JUMzJUEzbyU1Q1ZTJTIwQ29kZSU1Q1JlcG9zaXRvcmlvcyUyMEdpdCUyMEh1YiU1Q0xpbmtleSU1Q25vZGVfbW9kdWxlcyU1Q3JlbWl4aWNvbiU1Q2ZvbnRzJTVDcmVtaXhpY29uLmNzcyZzZXJ2ZXI9ZmFsc2UhIiwibWFwcGluZ3MiOiJBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8/ODRjZiJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQoLyogd2VicGFja01vZGU6IFwiZWFnZXJcIiAqLyBcIkM6XFxcXFVzZXJzXFxcXHdlbGxpXFxcXERvY3VtZW50c1xcXFxQcm9ncmFtYcOnw6NvXFxcXFZTIENvZGVcXFxcUmVwb3NpdG9yaW9zIEdpdCBIdWJcXFxcTGlua2V5XFxcXG5vZGVfbW9kdWxlc1xcXFxyZW1peGljb25cXFxcZm9udHNcXFxccmVtaXhpY29uLmNzc1wiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5Cwelli%5CDocuments%5CPrograma%C3%A7%C3%A3o%5CVS%20Code%5CRepositorios%20Git%20Hub%5CLinkey%5Cnode_modules%5Cremixicon%5Cfonts%5Cremixicon.css&server=false!\n"));
+
+/***/ }),
+
+/***/ "(app-pages-browser)/./node_modules/remixicon/fonts/remixicon.css":
+/*!****************************************************!*\
+  !*** ./node_modules/remixicon/fonts/remixicon.css ***!
+  \****************************************************/
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (\"b986945a0fa6\");\nif (true) { module.hot.accept() }\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9yZW1peGljb24vZm9udHMvcmVtaXhpY29uLmNzcyIsIm1hcHBpbmdzIjoiO0FBQUEsK0RBQWUsY0FBYztBQUM3QixJQUFJLElBQVUsSUFBSSxpQkFBaUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vbm9kZV9tb2R1bGVzL3JlbWl4aWNvbi9mb250cy9yZW1peGljb24uY3NzPzRiNjUiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgXCJiOTg2OTQ1YTBmYTZcIlxuaWYgKG1vZHVsZS5ob3QpIHsgbW9kdWxlLmhvdC5hY2NlcHQoKSB9XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/remixicon/fonts/remixicon.css\n"));
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["main-app"], function() { return __webpack_exec__("(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5Cwelli%5CDocuments%5CPrograma%C3%A7%C3%A3o%5CVS%20Code%5CRepositorios%20Git%20Hub%5CLinkey%5Cnode_modules%5Cremixicon%5Cfonts%5Cremixicon.css&server=false!"); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import './globals.css' // Se tiver esse, pode manter ou apagar
 import '../assets/scss/style.scss' // <--- AQUI o Next lê seu SASS todinho
 import 'remixicon/fonts/remixicon.css' // Se você instalou pelo npm
 
